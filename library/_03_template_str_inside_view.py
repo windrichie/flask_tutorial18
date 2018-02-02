f480cdafa8c9f818a7686d2cab66211d7832b4dc
Windrich <windrichie@gmail.com>
2018-02-02 17:43:13 +0800

    step3

--- library/_03_template_str_inside_view.py
+++ library/_03_template_str_inside_view.py
@@ -19,10 +19,12 @@ def hello_world():
     library_name = "Poe"
     html = """
         <html>
-            <h1>Welcome to {{library_name}} library!</h1>
+            <h1>Welcome to {{library}} library!</h1>
+            {% for author in authors %}
+                <li>{{ author }}</li>
+            {% endfor %}
         </html>
     """
-    rendered_html = render_template_string(html, library_name=library_name)
     authors = ["Alan Poe", "Jorge L. Borges", "Mark Twain"]
-    # Using an <ul> tag add the authors using the template engine
+    rendered_html = render_template_string(html, library=library_name, authors=authors)
     return rendered_html
